9c51ebbbde4e5c48f26ba65b71696c96a0f3f5da
Greg Wuller <greg@afofo.com>
2026-07-14 23:21:02 -0700

    feat(spray): add block DSP module and AVR32 app
    Introduce modules_block/spray as a block-based mixer and apps/spray as
    the companion AVR32 control app (forked from mix) with an embedded LDR.

---
+++ module_custom.h
@@ -0,0 +1,17 @@
+/* module_custom.h
+
+   some constants should be both customized for each module,
+   and preprocessed for efficiency.
+   here is where such things live.
+ */
+
+#ifndef _ALEPH_MODULE_CUSTOM_H_
+#define _ALEPH_MODULE_CUSTOM_H_
+
+#include "params.h"
+
+#define MODULE_BLOCKSIZE 16
+
+#define NUM_PARAMS eParamNumParams
+
+#endif // h guard
